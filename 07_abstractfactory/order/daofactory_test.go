6e76a7d31505d0cb802f81a8c3f63a001e0613f2
weibaohui <weibaohui@yeah.net>
2019-07-28 21:32:22 +0800

    抽象工厂模式

--- 07_abstractfactory/order/daofactory_test.go
+++ 07_abstractfactory/order/daofactory_test.go
@@ -3,12 +3,19 @@ package order
 import "testing"
 
 func TestRun(t *testing.T) {
+	// 使用XML抽象工厂
 	Run(&XmlDAOFactory{})
+	// 使用RDB抽象工厂
 	Run(&RdbDAOFactory{})
 }
+
+// 使用DAO的抽象工厂
 func Run(factory DAOFactory) {
+	// 通过抽象工厂获取需要的DAO接口
 	mainDAO := factory.CreateOrderMainDAO()
 	detailDAO := factory.CreateOrderDetailDAO()
+
+	// 调用DAO完成存储功能
 	mainDAO.SaveOrderMain()
 	detailDAO.SaveOrderDetail()
 }
