7318d6f5ea943910c5175f85bccf61a33d40cfd7
weibaohui <weibaohui@yeah.net>
2019-08-13 22:21:25 +0800

    策略模式

--- 17_strategy/price_test.go
+++ 17_strategy/price_test.go
@@ -9,5 +9,8 @@ func TestRun(t *testing.T) {
 	strategy := &LargeCustomerStrategy{}
 	price := NewPrice(strategy)
 	result := price.quote(7876)
+
+	price = NewPrice(&OldCustomerStrategy{})
+	result = price.quote(7655.32)
 	fmt.Println("报价", result)
 }
